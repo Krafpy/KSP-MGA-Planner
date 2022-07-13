08f5d824d0c382c0d98a3ea7bec7116f8080ae92
Krafpy <nazar.misyats@gmail.com>
2022-07-13 19:13:46 +0200

    Iterative hasNaN.

--- dist/dedicated-workers/libs/utils.js
+++ dist/dedicated-workers/libs/utils.js
@@ -2,19 +2,20 @@
 var Utils;
 (function (Utils) {
     function hasNaN(obj) {
-        for (const value of Object.values(obj)) {
-            if (typeof value == "object") {
-                if (hasNaN(value))
+        const stack = [obj];
+        while (stack.length > 0) {
+            const item = stack.pop();
+            for (const value of Object.values(item)) {
+                if (typeof value == "object") {
+                    stack.push(value);
+                }
+                else if (typeof value == "number" && isNaN(value)) {
                     return true;
-            }
-            else if (typeof value == "number") {
-                if (isNaN(value))
-                    return true;
+                }
             }
         }
         return false;
     }
     Utils.hasNaN = hasNaN;
-    ;
 })(Utils || (Utils = {}));
 ;
--- src/dedicated-workers/libs/utils.ts
+++ src/dedicated-workers/libs/utils.ts
@@ -5,7 +5,22 @@ namespace Utils {
      * @param obj The object to check
      * @returns true if it contains a NaN value
      */
-    export function hasNaN(obj: Object) {
+     export function hasNaN(obj: Object) {
+        const stack: Object[] = [obj];
+        while(stack.length > 0){
+            const item = stack.pop() as Object;
+            for(const value of Object.values(item)){
+                if(typeof value == "object"){
+                    stack.push(value);
+                } else if(typeof value == "number" && isNaN(value)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    /*export function hasNaN(obj: Object) {
         for(const value of Object.values(obj)){
             if(typeof value == "object"){
                 if(hasNaN(value))
@@ -16,5 +31,5 @@ namespace Utils {
             }
         }
         return false;
-    };
+    }*/
 };
